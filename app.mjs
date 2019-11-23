224f285f44de15cbdab01bed43f78a054ba8df58
CormoranCold <58109946+CormoranCold@users.noreply.github.com>
2019-11-23 18:35:49 +0100

    Uploaded web files

---
+++ app.mjs
@@ -0,0 +1,7 @@
+//app.mjs
+//import test from "./modules/modules.mjs";
+const dog="Papilon";
+const random_p = document.querySelector('.random > p');
+random_p.innerHTML = dog;
+mobileConsole.init();
+console.log('hiya');
---
+++ modules.mjs
@@ -0,0 +1,7 @@
+//modules.mjs
+const test = "This is a TEST string.";
+const randomTen = function() {
+  const ten = Math.floor(Math.random()*10);
+  return ten;
+}
+export default test;
