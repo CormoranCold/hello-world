e6b294db5516d075ad05f1bf4c97bf14960cb127
CormoranCold <58109946+CormoranCold@users.noreply.github.com>
2019-11-23 19:03:29 +0100

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,5 +1,5 @@
 //app.mjs
-import test from "./modules.mjs";
+import test from "modules.mjs";
 const dog="Papilon";
 const random_p = document.querySelector('.random > p');
 //random_p.innerHTML = dog;
