82f46d3ef97c132ed861babf1c9fbde723c568f8
CormoranCold <58109946+CormoranCold@users.noreply.github.com>
2019-11-24 06:43:46 +0100

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,8 +1,8 @@
 //app.mjs
-import test from "./modules.mjs";
+import randomTen from "./modules.mjs";
 const dog="Papilon";
 const cat = "The cat is Garfield";
 const random_p = document.querySelector('.random > p');
 //random_p.innerHTML = dog;
-random_p.innerHTML = test;
+random_p.innerHTML = randomTen();
 console.log('hiya');
