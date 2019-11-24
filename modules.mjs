fdd65a3f78f1634120eaeebdb2e56197e434f60e
CormoranCold <58109946+CormoranCold@users.noreply.github.com>
2019-11-24 06:40:40 +0100

    Update modules.mjs

--- modules.mjs
+++ modules.mjs
@@ -1,7 +1,7 @@
 //modules.mjs
-export const test = "This is a TEST string.";
+const test = "This is a TEST string.";
 const randomTen = function() {
   const ten = Math.floor(Math.random()*10);
   return ten;
 }
-//export default test;
+export default randomTen;
